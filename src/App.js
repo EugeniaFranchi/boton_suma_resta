a208165f1583694762b6860fe28316c2d55f27f9
Eugenia Franchi <mfranchi@fi.uba.ar>
2021-10-06 21:37:13 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  const decreaseByOne = () => setCounter(counter - 1);
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
+      <button onClick={decreaseByOne}>Minus</button>
+    </div>
+  );
+};
+
+export default App;
